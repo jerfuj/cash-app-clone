b9900898670fcfed421c2cc81fbdad726fd015dd
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-06-07 18:17:19 -1000

    Render different pages conditionally

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Sidebar from './sidebar/Sidebar';
+import styles from './App.module.css';
+import Activity from './Activity';
+import CashCard from './CashCard';
+import Settings from './Settings';
 
 function App() {
+  const [active, setActive] = useState('Activity');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.main}>
+      <Sidebar active={active} setActive={setActive} />
+      {active === 'Activity' && <Activity />}
+      {active === 'Cash Card' && <CashCard />}
+      {active === 'Settings' && <Settings />}
     </div>
   );
 }
